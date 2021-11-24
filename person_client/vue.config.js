463025e9691808f81e71cbf6288f361150d53bda
yangsong <2048400850@qq.com>
2021-11-24 17:07:27 +0800

    打包优化

--- person_client/vue.config.js
+++ person_client/vue.config.js
@@ -1,5 +1,13 @@
 console.log(process.env.NODE_ENV)
 
+const compressionWebpackPlugin = require('compression-webpack-plugin'); //引入插件
+const productionGZipExtensions = ['js', 'css']; //压缩的文件类型
+
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const {resolve} = require('path');
+
 
 module.exports = {
     // publicPath:process.env.NODE_ENV === 'production' ? '/vue_workspac/aihuhuproject/' : '/',
@@ -14,7 +22,7 @@ module.exports = {
     lintOnSave: true,
     // 生产环境是否生成 sourceMap 文件
     productionSourceMap: false,
-    // productionGzip: false,
+    // productionGzip: true,
     // productionGzipExtensions: ['js', 'css'],
     // css相关配置
     css: {
@@ -44,15 +52,67 @@ module.exports = {
         // }, 
         before: app => {}
     },
+
     chainWebpack(config) {
         // 去除console.log输出
         config.optimization.minimizer('terser').tap((args) => {
           args[0].terserOptions.compress.drop_console = true
           return args
         })
+        
+        // 开启gzip压缩
+        config.resolve.alias.set('@', resolve('src'));
+        if (process.env.NODE_ENV === 'production') {
+            config.plugin('compressionPlugin')
+            .use(new compressionWebpackPlugin({
+                filename: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: productionGZipExtensions,
+                threshold: 10240,
+                minRatio: 0.8,
+                deleteOriginalAssets: false
+            }));
+        }
     },
+    
+    configureWebpack:config=> {
+    
+        plugins: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                  //生产环境自动删除console
+                  compress: {
+                    drop_debugger: true,
+                    drop_console: true,
+                    pure_funcs: ['console.log']
+                  }
+                },
+                sourceMap: false,
+                parallel: true
+              }),
+    // 开启gzip压缩
+    //       new compressionWebpackPlugin({
+    //         //[file] 会被替换成原始资源。[path] 会被替换成原始资源的路径， [query] 会被替换成查询字符串。默认值是 "[path].gz[query]"。
+    //         asset: '[path].gz[query]', // 提示compression-webpack-plugin@3.0.0的话asset改为filename
+    //         //可以是 function(buf, callback) 或者字符串。对于字符串来说依照 zlib 的算法(或者 zopfli 的算法)。默认值是 "gzip"。
+    //         algorithm: 'gzip',
+    //         //所有匹配该正则的资源都会被处理。默认值是全部资源。
+    //         test: new RegExp('\\.(' + productionGZipExtensions.join('|') + ')$'),
+    //         //只有大小大于该值的资源会被处理。单位是 bytes。默认值是 0。
+    //         threshold: 10240,
+    //         //只有压缩率小于这个值的资源才会被处理。默认值是 0.8。
+    //         minRatio: 0.8,
+    //         //是否删除源资源
+    //         deleteOriginalAssets:false
+    //       })
+        ] 
+
+      },
+  
+
     // 第三方插件配置
     pluginOptions: {
         // ...
+        
     }
 };
